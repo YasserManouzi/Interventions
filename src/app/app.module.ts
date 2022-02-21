124ea10ed75a83aaa300a2495a23672024f54da2
YasserManouzi <yassermanouzi@outlook.com>
2022-02-21 16:08:00 -0500

    Version gérer les interventions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { RouterModule } from '@angular/router';
+import { ProblemeComponent } from './probleme/probleme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProblemeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
